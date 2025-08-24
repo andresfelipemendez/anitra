025064b0057f887d5237305f94247e0327bb3d28
Andrés Felipe Méndez <fmendez1212@gmail.com>
2025-08-24 01:06:27 -0500

    glad dll

--- src/externals/externals.h
+++ src/externals/externals.h
@@ -4,8 +4,7 @@
 #include <export.h>
 
 DECLARE_FUNC_INT_pGAME(init_externals) DECLARE_FUNC_VOID_pGAME(update_externals)
-    DECLARE_FUNC_VOID_pGAME(end_externals)
-
-        DECLARE_FUNC_VOID_pHOTRELOADABLE_IMGUI_DRAW(assign_hotreloadable)
+DECLARE_FUNC_VOID_pGAME(end_externals)
+DECLARE_FUNC_VOID_pHOTRELOADABLE_IMGUI_DRAW(assign_hotreloadable)
 
 #endif
